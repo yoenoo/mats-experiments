1166d14ede05758041de72cc98b430bb55ae156b
Yeonwoo Jang <yjang385@gmail.com>
2025-07-03 00:11:44 +0000

    working code for vllm

--- run.py
+++ run.py
@@ -1,71 +1,57 @@
 import re
+import asyncio
 from tqdm import tqdm
 from datasets import load_dataset
 from utils import load_model, chat, evaluate_solution
+from vllm_backend import run_vllm
 
-SYSTEM_PROMPT = """"""
 
-def main():
+
+
+if __name__ == "__main__":
   n_trials = 5  
 
   # model, tokenizer = load_model("Qwen/Qwen3-4B")
-  model, tokenizer = load_model("Qwen/Qwen2.5-Coder-7B-Instruct")
+  model_id = "Qwen/Qwen2.5-Coder-7B-Instruct"
+  model, tokenizer = load_model(model_id)
   dataset = load_dataset("google-research-datasets/mbpp", split="train")
   
   running_correct = 0
   running_total = 0
 
-  for sample in (t := tqdm(dataset)):
-    question = sample["text"]
-    test_lists = sample["test_list"]
+  results = asyncio.run(run_vllm(model_id, tokenizer, dataset, n_trials=n_trials))
+  print(results)
 
-    user_prompt = f"""
-You are an expert Python programmer, and here is your task: 
-  {question} 
-Your code should pass these tests:
-  {test_lists[0]}
 
-IMPORTANT: Your response must be in this exact format:
+#   for sample in (t := tqdm(dataset)):
+#     question = sample["text"]
+#     test_lists = sample["test_list"]
 
-<python>
-def function_name():
-    # your code here
-    pass
-</python>
+#     user_prompt = f"""
 
-Do NOT use markdown code blocks (```python). Use ONLY the <python> and </python> tags.
 
-Example of correct format:
-<python>
-def example():
-    return "hello world"
-</python>
-
-Now write your solution:
-    """
-
-    messages = [
-      {"role": "system", "content": SYSTEM_PROMPT},
-      {"role": "user", "content": user_prompt},
-    ]
+#     messages = [
+#       {"role": "system", "content": SYSTEM_PROMPT},
+#       {"role": "user", "content": user_prompt},
+#     ]
     
-    correct = False
-    # TODO: vLLM parallelize
-    # pass@5
-    for _ in range(n_trials):
-      completion = chat(model, tokenizer, messages=messages, max_tokens=1024)
-      # TODO: need a better parser
-      match = re.search(r"assistant.*?<python>(.*?)</python>", completion, re.MULTILINE | re.DOTALL)
-      if match:
-        guess = match.group(1)
-        correct = evaluate_solution(guess, test_lists)
-        if correct:
-          break
+#     correct = False
+#     # TODO: vLLM parallelize
+#     # pass@5
+#     for _ in range(n_trials):
+#       completion = chat(model, tokenizer, messages=messages, max_tokens=1024)
+#       # TODO: need a better parser
+#       match = re.search(r"assistant.*?<python>(.*?)</python>", completion, re.MULTILINE | re.DOTALL)
+#       if match:
+#         guess = match.group(1)
+#         correct = evaluate_solution(guess, test_lists)
+#         if correct:
+#           break
 
-    running_correct += correct
-    running_total += 1
-    t.set_description(f"Pass@{n_trials}: {running_correct}/{running_total}")
+#     running_correct += correct
+#     running_total += 1
+#     t.set_description(f"Pass@{n_trials}: {running_correct}/{running_total}")
 
 
-if __name__ == "__main__":
-  main()
+# if __name__ == "__main__":
+#   main()
---
+++ vllm_backend.py
@@ -0,0 +1,119 @@
+import os
+import re
+import ast
+import torch
+import pandas as pd
+from tqdm.asyncio import tqdm
+
+from transformers import AutoTokenizer
+from datasets import load_dataset
+
+import uuid
+from vllm import AsyncLLMEngine, AsyncEngineArgs, SamplingParams
+
+from dotenv import load_dotenv
+load_dotenv()
+
+# import argparse
+# parser = argparse.ArgumentParser()
+# parser.add_argument("--model_id", type=str, required=True)
+# FLAGS,_ = parser.parse_known_args()
+
+SYSTEM_PROMPT = """"""
+
+def create_prompt(question, test_lists, system_prompt=""):
+  user_prompt = f"""
+You are an expert Python programmer, and here is your task: 
+  {question} 
+Your code should pass these tests:
+  {test_lists[0]}
+
+IMPORTANT: Your response must be in this exact format:
+
+<python>
+def function_name():
+    # your code here
+    pass
+</python>
+
+Do NOT use markdown code blocks (```python). Use ONLY the <python> and </python> tags.
+
+Example of correct format:
+<python>
+def example():
+    return "hello world"
+</python>
+
+Now write your solution:
+  """
+
+  messages = [
+    {"role": "system", "content": system_prompt},
+    {"role": "user", "content": user_prompt},
+  ]
+  return messages
+
+async def run_vllm_inference(engine, tokenizer, question, test_lists, system_prompt="", max_tokens=1024, n=2, **kwargs):
+  sampling_params = SamplingParams(
+    n=n,                      # Number of completions to sample # n=1 is much slower?
+    max_tokens=max_tokens,
+    **kwargs,
+  )
+
+  messages = create_prompt(question, test_lists, system_prompt)
+  formatted_prompt = tokenizer.apply_chat_template(
+    messages,
+    tokenize=False,  # Return as string, not tokens
+    add_generation_prompt=True,  # Add assistant prompt for generation
+    temperature=1,
+    top_p=0.95,
+  )
+  generator = engine.generate(formatted_prompt, sampling_params, uuid.uuid4())
+
+  outputs = []
+  final_output = None 
+  async for output in generator:
+    final_output = output 
+
+  print("="*100)
+  print(len(final_output.outputs))
+  outputs = [final_output.outputs[i].text for i in range(n)]
+  
+  def parse_output(completion):
+    correct = False
+    match = re.search(r"assistant.*?<python>(.*?)</python>", completion, re.MULTILINE | re.DOTALL)
+    if match:
+      guess = match.group(1)
+      correct = evaluate_solution(guess, test_lists)
+    return correct
+
+  outputs = [parse_output(output) for output in outputs]
+  return outputs
+
+async def run_vllm(model, tokenizer, dataset, n_trials):
+  engine_args = AsyncEngineArgs(
+    model=model,
+    dtype="bfloat16", 
+    disable_log_requests=True,
+    disable_log_stats=True,
+    gpu_memory_utilization=0.6,
+  )
+  engine = AsyncLLMEngine.from_engine_args(engine_args)
+
+  tasks = [
+    run_vllm_inference(engine, tokenizer, example["text"], example["test_list"], system_prompt=SYSTEM_PROMPT, n=n_trials, max_tokens=1024, temperature=1.0, top_p=0.95)
+    for example in dataset
+  ]
+
+  results = []
+  for coroutine in tqdm.as_completed(tasks, total=len(dataset)):
+    results.append(await coroutine)  
+
+  return results
+
+# if __name__ == "__main__":
+#   from datasets import load_dataset
+#   train_dataset = load_dataset("predibase/wordle-grpo", split="train")
+
+#   import asyncio
+#   asyncio.run(main(train_dataset))
